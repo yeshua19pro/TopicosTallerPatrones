eb1a4f0bab67af354ce3e1ed742cab54e5ced206
yeshua19pro <yeshuaangelito@gmail.com>
2020-03-31 14:27:14 -0500

    prueba 5

---
+++ TallerPatrones/src/tallerpatrones/Main.java
@@ -0,0 +1,17 @@
+
+package tallerpatrones;
+
+/**
+ *
+ * @author Yashua
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
